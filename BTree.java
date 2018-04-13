788756647387273f9ae4085ceed9e4aafd986580
JoshuaPerkins <JoshuaPerkins@u.boisestate.edu>
2018-04-13 13:38:03 -0600

    Initial commit with basic class implementation files.

---
+++ BTree.java
@@ -0,0 +1,13 @@
+
+/**
+ * BTree class for ...
+ *
+ * @author JPerkins
+ * Date: April 13, 2018
+ *
+ * Class: CS 321 - Data Structures
+ * Spring 2018 - Steven Cutchin
+ */
+public class BTree {
+
+}
---
+++ GeneBankSearch.java
@@ -0,0 +1,17 @@
+
+/**
+ * GeneBankSearch class for ...
+ *
+ * @author JPerkins
+ * Date: April 13, 2018
+ *
+ * Class: CS 321 - Data Structures
+ * Spring 2018 - Steven Cutchin
+ */
+public class GeneBankSearch {
+
+    public static void main(String[] args) {
+
+    }
+
+}
