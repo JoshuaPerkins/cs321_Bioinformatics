7db52e3013a295ac7b636cea2273898ddb6f806e
JoshuaPerkins <JoshuaPerkins@u.boisestate.edu>
2018-04-14 12:56:38 -0600

    Updating GeneBankCreateBTree. Setting up passing in user arguments.
    Signed-off-by: JoshuaPerkins <JoshuaPerkins@u.boisestate.edu>

--- GeneBankCreateBTree.java
+++ GeneBankCreateBTree.java
@@ -13,6 +13,13 @@ public class GeneBankCreateBTree {
 
     public static void main(String[] args) {
 
+        final int MIN_ARGS_IN = 5;
+        final int MAX_ARGS_IN = 6;
+
+        final int OPTIMUM_DEGREE = 170;
+
+        final String DEBUG_FILE_OUT = "dump";
+
         // Defined DNA Bases
         final long BASE_A = 0b00L;
         final long BASE_T = 0b11L;
@@ -21,9 +28,104 @@ public class GeneBankCreateBTree {
 
         final int MAX_SEQUENCE_LENGTH = 31;
 
-        File gbk_file = new File(args[2]);
+        boolean useCache = false;
+        int treeDegree = 0;
+        int sequenceLength = 0;
+        int cacheSize = 0;
+        int debugLevel = 0;
+
+        if ((args.length < MIN_ARGS_IN) || (args.length > MAX_ARGS_IN)) {
+            System.out.println("ERROR: Incorrect arguments length; check usage.\n");
+            printProgramUse();
+        }
+
+        int index;
+
+        for (index = 0; index < args.length; index++) {
+            // try parsing and error dependent on index
+        }
 
         try {
+            // Set up sequence length from user input
+            if (Integer.parseInt(args[0]) == 1) {
+                useCache = true;
+            }
+            else if (Integer.parseInt(args[0]) == 0) {
+                useCache = false;
+            }
+            else {
+                System.out.println("ERROR: Check correct usage for <cache>.\n");
+                printProgramUse();
+            }
+        }
+        catch (NumberFormatException e) {
+            System.out.println("ERROR: Incorrect input for <cache> : " + e.getMessage() + "\n\n");
+            printProgramUse();
+        }
+
+        try {
+            // Set up sequence length from user input
+            treeDegree = Integer.parseInt(args[1]);
+            if (treeDegree < 0) {
+                System.out.println("ERROR: Check correct usage for <degree>.\n");
+                printProgramUse();
+            }
+            else if (treeDegree == 0) {
+                treeDegree = OPTIMUM_DEGREE;
+            }
+        }
+        catch (NumberFormatException e) {
+            System.out.println("ERROR: Incorrect input for <degree> : " + e.getMessage() + "\n\n");
+            printProgramUse();
+        }
+
+        try {
+            // Set up sequence length from user input
+            sequenceLength = Integer.parseInt(args[3]);
+            if ((sequenceLength < 1) || (sequenceLength > MAX_SEQUENCE_LENGTH)) {
+                System.out.println("ERROR: Check correct usage for <sequence length>.\n");
+                printProgramUse();
+            }
+        }
+        catch (NumberFormatException e) {
+            System.out.println("ERROR: Incorrect input for <sequence length> : " + e.getMessage() + "\n\n");
+            printProgramUse();
+        }
+
+        try {
+            // Set up sequence length from user input
+            cacheSize = Integer.parseInt(args[4]);
+            if (cacheSize < 1) {
+                System.out.println("ERROR: Check correct usage for <cache size>.\n");
+                printProgramUse();
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("ERROR: Incorrect input for <cache size> : " + e.getMessage() + "\n\n");
+            printProgramUse();
+        }
+
+        if (args.length > MIN_ARGS_IN) {
+            try {
+                // Set up sequence length from user input
+                debugLevel = Integer.parseInt(args[5]);
+                switch (debugLevel) {
+                    case 0:
+                        break;
+                    case 1:
+                        break;
+                    default:
+                        System.out.println("ERROR: Check correct usage for <cache size>.\n");
+                        printProgramUse();
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("ERROR: Incorrect input for <cache size> : " + e.getMessage() + "\n\n");
+                printProgramUse();
+            }
+        }
+
+        try {
+            File gbk_file = new File(args[2]);
+
             // Creates the File/Buffered Readers to process gbk file
             FileReader readFile = new FileReader(gbk_file);
             BufferedReader readBuffer = new BufferedReader(readFile);
@@ -38,7 +140,7 @@ public class GeneBankCreateBTree {
 
 
             // Reads through gbk file to parse out the sequences
-            while ((currentLine = readBuffer.readLine().trim()) != null) {  // toLower/Upper?
+            while ((currentLine = readBuffer.readLine().trim()) != null) {  // trim? toLower/Upper?
                 if (currentLine.startsWith("ORIGIN")) {
                     parseRun = true;
                 }
@@ -53,7 +155,7 @@ public class GeneBankCreateBTree {
                             currentChar = currentLine.charAt(currentLinePosition); // toLower/Upper?
                             currentLinePosition++;
 
-                            // Inserts into sequence
+                            // Inserts character into sequence
                             switch (currentChar) {
                                 case 'a':
                                     currentSequence = (currentSequence | (BASE_A << currentSequencePosition));
@@ -72,18 +174,22 @@ public class GeneBankCreateBTree {
                                     currentSequencePosition += 2;
                                     break;
                                 case 'n':
-                                    // Ends subsequence and continues parse
+                                    // Ends subsequence and resets parsing for a new sequence
                                     currentSequencePosition = 0;
                                     currentSequence = 0;
                                     continue;
                                 default:
+                                    // Skips white space and number characters
                                     continue;
+                            }
+                            if (currentSequencePosition >= sequenceLength) {
+                                // insert sequence
                             }
                         }
                     }
                 }
-
-
+                // Resets line position for new line input
+                currentLinePosition = 0;
             }
             // Closes the File/Buffer Readers
             readBuffer.close();
@@ -91,14 +197,52 @@ public class GeneBankCreateBTree {
         }
         // Catches exception if file not found
         catch(FileNotFoundException e) {
-            System.out.println("Cannot open file : " + e.getMessage() + "\n\n");
+            System.out.println("ERROR: Cannot open file : " + e.getMessage() + "\n\n");
             System.out.println("Make sure that the gbk file is in the same folder as the HashTest and other java files for this project.");
+            System.exit(0);
         }
         // Catches exception if error closing readers
         catch (IOException e) {
-            System.out.println("Error closing Buffer/File : " + e.getMessage() + "\n\n");
+            System.out.println("ERROR: When closing Buffer/File : " + e.getMessage() + "\n\n");
+            System.exit(0);
         }
 
+        // Checks debug level and prints to file accordingly
+        try {
+            if (debugLevel == 1) {
+                // my solution template
+//                // Saves the stdout to be reset afterwards
+//                PrintStream stdout = System.out;
+//                // Creates the output stream and sets the standard output to it
+//                PrintStream debugOut = new PrintStream(new FileOutputStream(DEBUG_FILE_OUT));
+//                System.setOut(debugOut);
+//                // Closes the output file
+//                debugOut.close();
+//                // Resets output to stdout
+//                System.setOut(stdout);
+
+                // Other solution
+                File dumpFile = new File(DEBUG_FILE_OUT);
+                dumpFile.delete();          // delete handle return?
+                dumpFile.createNewFile();   // create handle return?
+                PrintWriter writer = new PrintWriter(dumpFile);
+                //tree.inOrderPrintToWriter(tree.getRoot(),writer,sequenceLength);
+                writer.close();
+            }
+        }
+        // Catches exception if file not found
+        catch (FileNotFoundException e) {
+            System.out.println("Cannot open file : " + e.getMessage() + "\n\n");
+        }
+        catch (IOException e) {
+            System.out.println("ERROR: When closing Buffer/File : " + e.getMessage() + "\n\n");
+            System.exit(0);
+        }
     }
 
+    private static void printProgramUse() {
+        //System.out.println("Usage: java GeneBankCreateBTree <degree> <gbk file> <sequence length> [<debug level>]\n");
+        System.out.println("Usage: java GeneBankCreateBTree <cache> <degree> <gbk file> <sequence length> <cache size> [<debug level>]\n");   // cache optional?
+        System.exit(0);
+    }
 }
