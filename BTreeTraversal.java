66fc47f0a898fba34502b707373cd843003ff668
JoshuaPerkins <JoshuaPerkins@u.boisestate.edu>
2018-05-04 12:47:05 -0600

    Updated traversal to use getFreq.
    Signed-off-by: JoshuaPerkins <JoshuaPerkins@u.boisestate.edu>

--- BTreeTraversal.java
+++ BTreeTraversal.java
@@ -33,7 +33,7 @@ public class BTreeTraversal {
                 // Loops through objects until all keys and their frequencies are printed
                 for (int j = 0; j < node.getNumKeys(); j++) {
                     // Writes frequency and key to file then inserts a new line
-                    writer.print(node.getTreeObject(j).getFrequency() + " ");
+                    writer.print(node.getTreeObject(j).getFreq() + " ");
                     writer.print(GeneConvert.longToSubsequence(node.getTreeObject(j).getKey(), k));
                     writer.println();
                 }
