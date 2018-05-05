b81f9e5fe91592140e8d914ba4f19b105f8f84e3
JoshuaPerkins <JoshuaPerkins@u.boisestate.edu>
2018-05-04 19:12:51 -0600

    Updated Traverse
    Signed-off-by: JoshuaPerkins <JoshuaPerkins@u.boisestate.edu>

--- BTree.java
+++ BTree.java
@@ -251,6 +251,50 @@ public class BTree {
         return node;
     }
 
+    /**
+     * Inorder Traversal of the BTree; prints the frequency and key information from the nodes.
+     *
+     * @param node The BTree node to traverse and write information.
+     * @param k The subsequence length.
+     * @param writer The PrintWriter
+     * @throws IOException
+     */
+    public void inorderDebugPrinter(BTreeNode node, int k, PrintWriter writer) throws IOException {
+        if (node != null) {
+            for (int i = 0; i < node.numChildren; i++) {
+                // Temporarily store the child node
+                BTreeNode temp = readNode(node.children[i]);
+                // If node is not leaf go to child
+                if (!node.isLeaf) {
+                    // Recursive call to traverse
+                    inorderDebugPrinter(temp, k, writer);
+                }
+                // Loops through objects until all keys and their frequencies are printed
+                for (int j = 0; j < node.numKeys; j++) {
+                    // Writes frequency and key to file then inserts a new line
+                    writer.print(node.keys[i].getFreq() + " ");
+                    writer.print(GeneConvert.longToSubsequence(node.keys[i].getKey(), k));
+                    writer.println();
+                }
+            }
+        }
+//        for (int i = 0; i < node.numKeys; i++){
+//            writer.print(node.keys[i].getFreq()+ " ");
+//            writer.println(GeneConvert.longToSubsequence(node.keys[i].getKey(), k));
+//        }
+//        if (!node.isLeaf()){
+//            for (int i = 0; i < node.numKeys + 1; ++i){
+//                int offset = node.children[i];
+//                BTreeNode x = readNode(offset);
+//                inorderDebugPrinter(x, k, writer);
+//                if (i < node.numKeys) {
+//                    writer.print(node.keys[i].getFreq() + " ");
+//                    writer.println(GeneConvert.longToSubsequence(node.keys[i].getKey(),k));
+//                }
+//            }
+//        }
+    }
+
     class BTreeNode {
         // class variables
         int fileOffset;
